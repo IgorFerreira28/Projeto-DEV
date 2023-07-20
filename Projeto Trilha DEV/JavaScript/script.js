776f5bd0bf9b85d6d0f8f33fa52530beae7f080a
Igor Ferreira <igorferreirafonseca@gmail.com>
2023-07-20 17:22:22 -0300

    Ajustes finais do CSS e do formulário

--- Projeto Trilha DEV/JavaScript/script.js
+++ Projeto Trilha DEV/JavaScript/script.js
@@ -3,6 +3,8 @@ const nameInput = document.querySelector("#nome")
 const emailInput = document.querySelector("#email")
 const linkInput = document.querySelector("#linkport")
 const respostaTextarea = document.querySelector("#resposta")
+const modal = document.querySelector("#modal")
+
 
 
 form.addEventListener("submit", (event) => {
@@ -27,7 +29,29 @@ form.addEventListener("submit", (event) => {
         alert("Por Favor, escreva os motivos para fazer parte da jojos")
     }
 
-    form.submit();
+    function showModal() {
+        const modal = document.getElementById("modal");
+        const fade = document.getElementById("fade");
+        modal.classList.remove("hide");
+        fade.classList.remove("hide");
+    }
+
+    
+    function hideModal() {
+        const modal = document.getElementById("modal");
+        const fade = document.getElementById("fade");
+        const form = document.getElementById("form");
+        form.reset(); 
+        modal.classList.add("hide");
+        fade.classList.add("hide");
+    }
+
+    showModal();
+
+    const modal = document.getElementById("modal");
+    const fade = document.getElementById("fade");
+    modal.addEventListener("click", hideModal);
+    fade.addEventListener("click", hideModal);
 });
 
 
